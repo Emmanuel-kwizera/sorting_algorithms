7ab89f7d99037575ea52980d277525f945017e14
Natusino <kwizemmy2004@gmail.com>
2021-06-15 21:29:59 +0200

    removing comments

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -4,25 +4,17 @@ using namespace std;
 void merge(int arr[], int l, int m, int r){
     int n1 = m - l + 1;
     int n2 = r - m;
- 
-    // Create temp arrays
+
     int L[n1], R[n2];
  
-    // Copy data to temp arrays L[] and R[]
     for (int i = 0; i < n1; i++)
         L[i] = arr[l + i];
     for (int j = 0; j < n2; j++)
         R[j] = arr[m + 1 + j];
- 
-    // Merge the temp arrays back into arr[l..r]
- 
-    // Initial index of first subarray
-    int i = 0;
- 
-    // Initial index of second subarray
+
+
     int j = 0;
  
-    // Initial index of merged subarray
     int k = l;
  
     while (i < n1 && j < n2) {
@@ -37,16 +29,12 @@ void merge(int arr[], int l, int m, int r){
         k++;
     }
  
-    // Copy the remaining elements of
-    // L[], if there are any
     while (i < n1) {
         arr[k] = L[i];
         i++;
         k++;
     }
  
-    // Copy the remaining elements of
-    // R[], if there are any
     while (j < n2) {
         arr[k] = R[j];
         j++;
